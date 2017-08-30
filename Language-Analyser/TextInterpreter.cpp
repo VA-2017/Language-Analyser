f454ca3d1ed809db75b768b32473d58f9926e7e2
Lord-Curdin <lucuhu@gmail.com>
2017-08-30 16:59:14 +0200

    Add some Basic Things

--- Language-Analyser/TextInterpreter.cpp
+++ Language-Analyser/TextInterpreter.cpp
@@ -2,24 +2,62 @@
 
 TextInterpreter::TextInterpreter(std::string ImportText)
 {
+    Text = ImportText;
+    CountAllLetters();
+    DeleteAllNonProcessedLetters();
 }
 
 void TextInterpreter::DeleteAllNonProcessedLetters()
 {
+
+    std::invalid_argument("Nicht verarbeitbare Zeichen im Text.");
+
 }
 
-void TextInterpreter::CountAllSpecialKeys()
+bool TextInterpreter::CountAllCTRLATLKeys()
 {
+    std::exception("Nicht Implementiert");
+    return false;
 }
 
-void TextInterpreter::CountAllCTRLATLKeys()
+bool TextInterpreter::CountAllShiftKeys()
 {
+    std::exception("Nicht Implementiert");
+    return false;
 }
 
-void TextInterpreter::CountAllShiftKeys()
+bool TextInterpreter::CountNormalLetters()
 {
+    return false;
+}
+
+bool TextInterpreter::ExtractAllNumbers()
+{
+    return false;
+}
+
+bool TextInterpreter::CompareLetter(std::vector<Letter> CurrentComparable)
+{
+    for (size_t i = 0; i < CurrentComparable.size(); i++) {
+        if (*CurrentComparable.at(i).specificLetter.c_str() == (Text.at(LetterCount))) {
+            CurrentComparable.at(i).Count++;
+        }
+    }
+    return false;
 }
 
 void TextInterpreter::CountAllLetters()
 {
+    for (size_t i = 0; i < Text.size(); i++) {
+        if (CountAllCTRLATLKeys()) {
+            continue;
+        }
+        if (CountAllShiftKeys()) {
+            continue;
+        }
+        if (CountNormalLetters()) {
+
+        }
+        DeleteAllNonProcessedLetters();
+    }
 }
--- Language-Analyser/TextInterpreter.h
+++ Language-Analyser/TextInterpreter.h
@@ -2,23 +2,34 @@
 #include <iostream>
 #include <vector>
 #include <map>
+
 using namespace std;
-class TextInterpreter
-{
+
+struct Letter {
+    string specificLetter;
+    unsigned long Count;
+};
+
+class TextInterpreter {
 public:
     TextInterpreter(string ImportText);
 
 private:
-    void DeleteAllNonProcessedLetters();   
-    void CountAllSpecialKeys();
-    void CountAllCTRLATLKeys();
-    void CountAllShiftKeys();
+    void DeleteAllNonProcessedLetters();
     void CountAllLetters();
+
+    bool CountAllCTRLATLKeys();
+    bool CountAllShiftKeys();
+    bool CountNormalLetters();
+    bool ExtractAllNumbers();
+    bool CompareLetter(std::vector<Letter> CurrentComparable);
 public:
 
 private:
     string Text;
-    map<char, unsigned long>CTRLALTLetters;
-    map<char,unsigned long>ShiftLetters;
-    map<char,unsigned long>Letters;
+    size_t LetterCount = 0;
+    vector<Letter>CTRLALTLetters;
+    vector<Letter>ShiftLetters;
+    vector<Letter>Letters {{"a", 0}, {"b", 0}, {"c", 0}, {"d", 0}, {"e", 0}, {"f", 0}, {"g", 0}, {"h", 0}, {"j", 0}, {"k", 0}, {"l", 0}, {"m", 0}, {"n", 0}, {"o", 0}, {"p", 0}, {"q", 0}, {"r", 0}, {"s", 0}, {"t", 0}, {"u", 0}, {"v", 0}, {"w", 0}, {"x", 0}, {"y", 0}, {"z", 0}};
 };
+
